70a5dc6b9d331010aa7051a76d4b0278ce77ffcf
babsmutini <scotteyawo@gmail.com>
2022-06-02 13:05:23 -0700

    betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
-
-int main(void){
+/**
+ *main - This is the main function, returns 0 if true
+ *Return : ALways 0 (Sucess)
+ *
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
