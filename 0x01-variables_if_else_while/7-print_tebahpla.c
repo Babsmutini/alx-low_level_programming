a4b086bd9823e3bd93379062bbb67aaff33ff6a3
Babsmutini <scotteyawo@gmail.com>
2022-06-07 05:40:23 +0100

    if else completed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	char uppalpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int n;
+
+	for (n = 25; n >= 0; n--)
+	{
+		putchar(uppalpha[n]);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
