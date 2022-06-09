66a94354fd41c9c176080be0f4991748f44c0183
Babsmutini <scotteyawo@gmail.com>
2022-06-09 05:36:44 +0100

    A lot of tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of any integer
+ *
+ * @a : integer number
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-1 * a);
+	}
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a given integer
+ *
+ * @a : integer 
+ * Return: Always 0
+ */
+int print_last_digit(int a)
+{
+	int n;
+
+	n = a % 10;
+	if (n < 0)
+	{
+		_putchar(-n + 48);
+		return (-n);
+	}
+	else
+	{
+		_putchar(n + 48);
+		return (n);
+	}
+}
