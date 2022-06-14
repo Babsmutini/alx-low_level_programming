a12d1419e62f286394170a9dc421a69bdc63e654
Babsmutini <scotteyawo@gmail.com>
2022-06-14 01:59:34 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - idk
+ *
+ * @str : pointer
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int size = strlen(str);
+
+	int i = 0;
+
+	while (i < size)
+	{
+		putchar(*(str + i));
+		i = i + 2;
+	}
+	putchar('\n');
+}å
