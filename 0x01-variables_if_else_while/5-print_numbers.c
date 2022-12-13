6d7a981b16fa2133c0bd303b6f68a9d693a4f592
babsmutini <scotteyawo@gmail.com>
2022-12-12 17:03:35 -0800

    Print numbers with printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print all single digit numbers starting from 0
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+	printf("%d", i);
+return (0);
+}
