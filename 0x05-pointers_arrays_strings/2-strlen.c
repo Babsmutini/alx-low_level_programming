6d46352460db941417759ea069dec5147937c155
babsmutini <scotteyawo@gmail.com>
2022-12-20 18:30:48 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ *
+ * @s: pointer to string
+ *
+ * Return: Length of string
+ *
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != "\0")
+	{
+		i++;
+	}
+	return (i);
+}
+
