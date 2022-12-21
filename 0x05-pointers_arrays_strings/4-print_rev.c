5a8e37315f3824c2f975838ff2ee274d108b853a
babsmutini <scotteyawo@gmail.com>
2022-12-20 19:22:36 -0800

    changed putchar

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 #include "string.h"
 /**
  * print_rev - prints string in reverse
@@ -14,7 +15,7 @@ int len = strlen(s);
 int i = 0;
 for (i = len - 1; i >= 0; i--)
 {
-_putchar(s[i]);
+putchar(s[i]);
 }
-_putchar("\n");
+putchar("\n");
 }
